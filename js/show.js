53df0e1368f9f0220189ba57219ffc8cc8de6edb
“4slash271” <4slash271@gmail.com>
2021-11-25 23:52:33 +0900

    editing side nav var

--- js/show.js
+++ js/show.js
@@ -1,15 +1,20 @@
-// const nav = document.querySelector(".nav-bar");
-const button = document.querySelector(".nav-bar--show");
+const nav = document.querySelector(".nav-bar");
+const navBtn = document.querySelector(".nav-bar-btn");
 const chosenImg = document.querySelector("#chosenImg");
 
 function showImage(event){
-    button.classList.add("open-show-btn");
-    chosenImg.innerHTML = '<img id="show--img" src="../img/cat_meme.png">';
+    nav.classList.add("open-show");
+    navBtn.classList.add("open-btn");
+    navBtn.classList.remove("hide-btn");
+    chosenImg.innerHTML = '<img id="show-img" src="../img/cat_meme.png">';
 }
 function hideImage(){
+    nav.classList.remove("open-show");
+    navBtn.classList.remove("open-btn");
+    navBtn.classList.add("hide-btn");
     chosenImg.innerHTML = '';
 }
 
 
-button.addEventListener("mouseover",showImage);
-button.addEventListener("mouseout",hideImage);
+navBtn.addEventListener("mouseover",showImage);
+navBtn.addEventListener("mouseout",hideImage);
